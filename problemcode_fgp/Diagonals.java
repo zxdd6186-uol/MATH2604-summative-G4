975dcce3c6db09ec83d394997a592da1748e2cd0
zxdd6186-uol <zxdd6186@leeds.ac.uk>
2026-03-11 14:41:36 +0000

    Created code for the ExampleMatrix task for Diagonals

--- problemcode_fgp/Diagonals.java
+++ problemcode_fgp/Diagonals.java
@@ -1,6 +1,8 @@
-import static java.lang.Math.*;
+class Diagonals {
 
-class Diagonals
-{
- 
+    public static double[] exampleMatrix() {
+        double[] a = {10, 8, 5, -10, 7}; // creates the array for the diagonal elements of the matrix
+        return a; //returns the array which represents the diagonal for the matrix
+    }
+
 }
