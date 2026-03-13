e39deed9b582f14e656fe83cb40a8162a28bcfc6
zxdd6186-uol <zxdd6186@leeds.ac.uk>
2026-03-13 15:18:42 +0000

    making code more streamline/ efficient

--- problemcode_fgp/Tridiagonals.java
+++ problemcode_fgp/Tridiagonals.java
@@ -15,14 +15,8 @@ static double[][] exampleMatrix(int n)
         if (a.length != 3) { // there are 3 diagonals in the matrix or 3 'rows' in the storage array
             return false;
         }
-        if (a[0] == null) {
-            return false;
-        }
-        if (a[1] == null) {
-            return false;
-        }
-        if (a[2] == null) { // checking none of the diagonals are null
-            return false;
+        if (a[0] == null || a[1] == null || a[2] == null) {
+        return false;
         }
         int n = a[0].length; // n is the length of each of the storage rows 
         if (n<1) { // checks if the row length is too small (0)
