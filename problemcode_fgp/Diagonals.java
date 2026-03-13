0b456673f3ae1b2f3bbdb10ffaaf9625f86077b7
Robert Bassett <gqvv2063@leeds.ac.uk>
2026-03-13 18:00:50 +0000

    Added Javacode comments for the inverse method

--- problemcode_fgp/Diagonals.java
+++ problemcode_fgp/Diagonals.java
@@ -7,6 +7,15 @@ class Diagonals {
         return a; //returns the array which represents the diagonal for the matrix
     }
 
+/**
+ * Computes the inverse of a diagonal matrix by calculating the reciprocal of each diagonal entry.
+ * 
+ * @param a an array which contains the diagonal entries of the matrix
+ * @return an array which contains the reciprocals of each of the diagonal entries from the input matrix,
+ * or null in the exceptional case wherein the input array is null
+ * 
+ * Assumes that the matrix is invertible, none of the diagonal entries are zero
+ */
     public static double[] inverse(double[] a) {
         if (a == null) {
             return null;
